5e19878d999a37751a450f173943cfbba7e2ab8b
Lucas Crespo <lucres13pgr@gmail.com>
2022-10-18 19:09:11 -0300

    gitignoreChange

--- commands.py
+++ commands.py
@@ -7,6 +7,6 @@ class commands():
 
     def addCommand(self):
         def inner(func, *args, **kwargs):
-            self.events[func.__name__] = (func," - {} {}:{}\n".format(func.__name__, str(inspect.getfullargspec(func).args).replace("[]", ""), func.__doc__))
+            self.events[func.__name__] = (func," - {} {}:{}\n".format(func.__name__, str(inspect.getfullargspec(func).args).replace("[]", "")," " + func.__doc__))
         return inner
 
